2d9eb0b577ec3d6f730d3a6b8868676ca6325eeb
6ixFeetWinter <k020g1278@g.neec.ac.jp>
2022-05-24 11:10:00 +0900

    kk

---
+++ asset/switcher-Bt.js
@@ -0,0 +1,17 @@
+const pc = document.querySelector('.pc-button');
+const sp = document.querySelector('.sp-button');
+const desgin = document.querySelector('.desgin');
+pc.addEventListener('click',()=>{
+    pc.classList.add('switch-active');
+    sp.classList.remove('switch-active');
+    desgin.innerHTML='<picture> <source src="../images/Works/pasta/pcFull-pasta.avif"> <img src="../images/Works/pasta/pcFull-pasta.webp" load="lazy" alt="制作したデザイン"></picture>';
+    desgin.classList.remove('sp-size');
+});
+sp.addEventListener('click',()=>{
+    sp.classList.add('switch-active');
+    pc.classList.remove('switch-active');
+    desgin.innerHTML='<picture> <source src="../images/Works/pasta/spFull-pasta.avif"> <img src="../images/Works/pasta/spFull-pasta.webp" load="lazy" alt="制作したデザイン"></picture>';
+    desgin.classList.add('sp-size');
+});
+
+
